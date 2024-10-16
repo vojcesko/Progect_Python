89d2adb1c2e887b4b85c20e651024037a784d615
Ihor Voitseshko <vojceskoigor@gmail.com>
2024-10-16 15:56:23 +0300

    Bash

--- app.py
+++ app.py
@@ -5,5 +5,11 @@ app= Flask(__name__)
 def main():
         return 'Hello, world!'
 
+@app.route('/homepage') 
+def home():
+    """View for the Home page of your website."""
+
+    return "This is your homepage :) "
+
 if __name__ == '__main__':
-        app.run(debug=True)
+        app.run()
