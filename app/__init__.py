1e0c7adebbbb353db18261257fda75af1f21404a
Ihor Voitseshko <vojceskoigor@gmail.com>
2024-11-06 11:27:56 +0200

    cooki

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from utils.config import get
 app = Flask(__name__)
 app.debug = get("FLASK_DEBUG", int) or False
+app.secret_key = 'SECRET_KEY'
 
 from .users import users_blueprint
 from .views import main, resume, contact
--- app/users/views.py
+++ app/users/views.py
@@ -1,5 +1,9 @@
+from datetime import timedelta
+from flask import (render_template, request, url_for, redirect, flash, session, make_response)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
 from . import users_blueprint
-from flask import render_template, request, url_for, redirect
 
 @users_blueprint.route('/hi/<string:name>')
 def greetings(name):
@@ -13,3 +17,87 @@ def admin():
     to_url = url_for("users.greetings", name="administrator", age=19, _external=True)
     print(to_url)
     return redirect(to_url)
+
+
+@users_blueprint.route("/login", methods=["GET", "POST"])
+def login():
+    correct_username = "Voitseshko"
+    correct_password = generate_password_hash("12345678")
+
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        is_username_correct = username == correct_username
+        is_password_correct = check_password_hash(correct_password, password)
+        
+        if is_username_correct and is_password_correct:
+            session["user"] = username
+            flash("Login successful!", "success")
+            return redirect(url_for("users.profile", name=username))
+        else:
+            flash("Invalid username or password", "danger")
+    return render_template("login.html")
+
+
+@users_blueprint.route("/profile", methods=["GET", "POST"])
+def profile():
+    if "user" not in session:
+        flash("You need to log in to view this page.", "warning")
+        return redirect(url_for("users.login"))
+    
+    if request.method == "POST":
+        action = request.form.get("action")
+
+        if action == "add":
+            key = request.form.get("key")
+            value = request.form.get("value")
+            days = request.form.get("days", type=int)
+
+            if key and value and days:
+                response = make_response(redirect(url_for("users.profile")))
+                response.set_cookie(key, value, max_age=timedelta(days=days))
+                flash(f"Cookie '{key}' added successfully!", "success")
+                return response
+            else:
+                flash_message = (
+                        "Please provide a valid key, "
+                        "value and expiration days."
+                )
+                flash(flash_message, "danger")
+        elif action == "delete_key":
+            key = request.form.get("delete_key")
+            response = make_response(redirect(url_for("users.profile")))
+            response.set_cookie(key, "", expires=0)
+            flash(f"Cookie '{key}' deleted successfully!", "success")
+            return response
+        
+        elif action == "delete_all":
+            response = make_response(redirect(url_for("users.profile")))
+            for cookie_key in request.cookies.keys():
+                response.set_cookie(cookie_key, "", expires=0)
+            flash("All cookies deleted successfully!", "success")
+            return response
+        
+    current_cookies = request.cookies.to_dict()
+    return render_template("profile.html", username=session["user"],
+                           cookies=current_cookies)
+
+
+@users_blueprint.route("/logout", methods=["POST"])
+def logout():
+    session.pop("user", None)
+    flash("You have benn logged out.", "info")
+    return redirect(url_for("users.login"))
+
+
+@users_blueprint.route("/set_color_scheme/<string:scheme>")
+def set_color_scheme(scheme):
+    if scheme not in ["light", "dark"]:
+        flash("Invalid color scheme selected.", "danger")
+        return redirect(url_for("users.profile"))
+    
+    response = make_response(redirect(url_for("users.profile")))
+    response.set_cookie("color_scheme", scheme, max_age=timedelta(days=30))
+    flash(f"Color scheme set to {scheme.capitalize()}!", "success")
+    return response
